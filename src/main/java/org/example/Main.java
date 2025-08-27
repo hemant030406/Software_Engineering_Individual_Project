14982da45fb04f9c8ff33d1894eda6f74ba2f6a0
hemant030406 <112201024@smail.iitpkd.ac.in>
2025-08-27 20:41:47 +0530

    usecase added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,16 @@ public class Main {
      * @param args Command-line arguments (not used in this application).
      */
     public static void main(final String[] args) {
+        final ILogger consoleLogger = new ConsoleLogger();
+        consoleLogger.log("Hello World!");
 
+        final ILogger fileLogger = new FileLogger("./fileLog.txt");
+        fileLogger.log("Hello World!");
+
+        final ILogger newtworkLogger = new NetworkLogger("log.server.com", 8000);
+        newtworkLogger.log("Hello World!");
+
+        final ILogger nullLogger = new NullLogger();
+        nullLogger.log("Hello World!");
     }
 }
